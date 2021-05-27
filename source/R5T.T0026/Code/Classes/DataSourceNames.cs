0be16d1a425e7aceef2185e5a5e04336e751ba10
David Coats <david@getsafetycone.com>
2021-05-27 10:46:28 -0700

    Add IDataSourceNames interface for use as a base for extension methods. Add simple implementation.

---
+++ source/R5T.T0026/Code/Classes/DataSourceNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.T0026
+{
+    public class DataSourceNames : IDataSourceNames
+    {
+        #region Static
+
+        public static DataSourceNames Instance { get; } = new DataSourceNames();
+
+        #endregion
+    }
+}
---
+++ source/R5T.T0026/Code/Interfaces/IDataSourceNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.T0026
+{
+    /// <summary>
+    /// Empty interface to be used as a base for extension methods.
+    /// </summary>
+    public interface IDataSourceNames
+    {
+    }
+}
